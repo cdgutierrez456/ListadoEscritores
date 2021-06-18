e28767e6e784c3d7b58e2fa3c78fa1b0ab5b2b2b
Cristian Gutierrez <cdgutierrez456@gmail.com>
2021-06-18 08:19:22 -0500

    Configuracion componente lista-escritores haciendo uso de la base de datos simulada

---
+++ src/app/lista-escritores/lista-escritores.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { EscritoresService } from '../escritores.service';
+import { Escritor } from '../models/escritor.model';
+
+@Component({
+  selector: 'app-lista-escritores',
+  templateUrl: './lista-escritores.component.html',
+  styleUrls: ['./lista-escritores.component.css']
+})
+export class ListaEscritoresComponent implements OnInit {
+
+  arrEscritores: Escritor[];
+
+  constructor(private escritoresService: EscritoresService) { }
+
+  ngOnInit(): void {
+    this.arrEscritores = this.escritoresService.getAll();
+  }
+
+}
