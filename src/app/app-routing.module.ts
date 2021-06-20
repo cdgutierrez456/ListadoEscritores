62cad88a2b0125758aa5c51baafa821fb03e3c6a
Cristian Gutierrez <cdgutierrez456@gmail.com>
2021-06-19 22:22:36 -0500

    Asociando ruta hija /libros a componente escritor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DetalleComponent } from './detalle/detalle.component';
 import { ListaEscritoresComponent } from './lista-escritores/lista-escritores.component';
+import { ListaLibrosComponent } from './lista-libros/lista-libros.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/escritores' },
   { path: 'escritores', component: ListaEscritoresComponent },
-  { path: 'escritores/:escritorId', component: DetalleComponent },
+  { 
+    path: 'escritores/:escritorId', component: DetalleComponent, children: [
+      { path: 'libros', component: ListaLibrosComponent }
+    ] 
+  },
   { path: '**', redirectTo: '/escritores' }
 ];
 
